5848862edfa95ff5837de63463b2aa10a61472fe
Lucas Montenegro <144347120+lukinhas563@users.noreply.github.com>
2024-03-13 14:58:25 -0300

    Update app-config.ts

--- src/config/app-config.ts
+++ src/config/app-config.ts
@@ -2,4 +2,4 @@ export const API_URL = 'https://strapi-test-ocrw.onrender.com';
 export const POSTS_URL = `${API_URL}/api/posts?populate=*`;
 
 export const SITE_NAME = 'Blog Strapi';
-export const SITE_URL = 'https://blog-strapi-test.netlify.app';
+export const SITE_URL = 'https://blog-strapi-test.vercel.app';
